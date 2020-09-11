b70c337087993a8f2fc9fd3dd178dff29de9e715
Colin Zhao <colinzhao777@gmail.com>
2020-09-11 11:54:45 -0700

    change install

--- translateSC/files/run.sh
+++ translateSC/files/run.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+if [[ ! -d venv ]]
+then
+  pip3 install virtualenv
+  virtualenv venv 
+  source venv/bin/activate
+  pip install -r requirements.txt
+  deactive
+  cp chi_tra.traineddata /usr/local/share/tessdata
+  cp chi_sim.traineddata /usr/local/share/tessdata
+fi
+
 source venv/bin/activate
 
 echo "This script requires admin priviliges to run. Please type in your password to allow privilges. If you do not want to do this, press ctrl c"
