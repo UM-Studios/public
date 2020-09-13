03799a63e1bc810e21a4254dcc4d4361f114a3c4
Colin Zhao <colinzhao777@gmail.com>
2020-09-12 21:15:14 -0700

    install options

--- GoogleTranslit/mac/files/install.sh
+++ GoogleTranslit/mac/files/install.sh
@@ -3,39 +3,56 @@
 which -s brew
 if [[ $? != 0 ]]
 then
-    echo "This script will need to install homebrew. If you are unsure what homebrew is, you can get a quick overview here: https://techstacker.com/what-is-homebrew/. If you will allow this script to install homebrew, type yes. Otherwise, type no. [yes/no]"
+  echo "This script will need to install homebrew. If you are unsure what homebrew is, you can get a quick overview here: https://techstacker.com/what-is-homebrew/. If you will allow this script to install homebrew, type yes. Otherwise, type no. [yes/no]"
 
-    while [[ true ]]
-    do
-        read agree
-        if [[ $agree == "no" ]]
-        then
-            exit 0
-        elif [[ $agree == "yes" ]]
-        then
-            break
-        else
-            echo "Please enter yes or no."
-        fi
-
-    done
-    if /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  while [[ true ]]
+  do
+    read agree
+    if [[ $agree == "no" ]]
     then
-        echo "Installed homebrew successfully"
+      exit 0
+    elif [[ $agree == "yes" ]]
+    then
+      break
     else
-        echo "Oops! looks like something went wrong with the download. Please read the error message and try to download homebrew yourself. Or, restart this script and see if it works then."
-        exit 1
+      echo "Please enter yes or no."
     fi
-    printf '\n\n\n\n'
+
+  done
+  if /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  then
+    echo "Installed homebrew successfully"
+  else
+    echo "Oops! looks like something went wrong with the download. Please read the error message and try to download homebrew yourself. Or, restart this script and see if it works then."
+    exit 1
+  fi
+  printf '\n\n\n\n'
 fi
 
 brew install python
 brew install tesseract
 brew install wget
 
+echo "This script needs to install data files for our screenshot character recognition to work. Would you like to install better data for higher accuracy but longer download times, or faster data for less accuracy but faster download times? Type better for better data or faster for faster data [better/faster]"
+
+while [[ true ]]
+do
+  read which_data
+  if [[ $which_data == "better" ]]
+  then
+    data = "best"
+    break
+  elif [[ $which_data == "faster" ]]
+  then
+    data = "fast"
+    break
+  else
+    echo "Please enter either better or faster"
+  fi
+done
 
 while IFS=, read -r code name; do
-  wget https://github.com/tesseract-ocr/tessdata_best/raw/master/$code.traineddata
+  wget https://github.com/tesseract-ocr/tessdata_$data/raw/master/$code.traineddata
   mv $code.traineddata /usr/local/share/tessdata
 done < language_codes.csv
 
